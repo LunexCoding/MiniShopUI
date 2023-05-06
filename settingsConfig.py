d275ef19830b33a62274d1aab617819672fd2355
LunexCoding <lunexcoding@vk.com>
2023-05-06 22:35:34 +0700

    Add settings config

---
+++ settingsConfig.py
@@ -0,0 +1,34 @@
+from decouple import config
+
+
+class _SettingsConfig:
+    def __init__(self):
+        self.__settingsConfig = self.__loadSettings()
+
+    def __loadSettings(self):
+        __settings = {}
+        __settings["DATABASE"] = dict(
+            host=config("DB_HOST"),
+            port=config("DB_PORT", cast=int),
+            user=config("DB_USER"),
+            password=config("DB_PASSWORD"),
+            database=config("DB_NAME")
+        )
+        __settings["SMTP"] = dict(
+            host=config("SMTP_HOST"),
+            port=config("SMTP_PORT", cast=int),
+            email=config("SMTP_EMAIL"),
+            password=config("SMTP_PASSWORD")
+        )
+        return __settings
+
+    @property
+    def DatabaseSettings(self):
+        return self.__settingsConfig["DATABASE"]
+
+    @property
+    def SMTPSettings(self):
+        return self.__settingsConfig["SMTP"]
+
+
+settingsConfig = _SettingsConfig()
