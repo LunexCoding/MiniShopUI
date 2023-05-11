deba3761f47a6ec52000a54704027ef4a6f3b359
LunexCoding <lunexcoding@vk.com>
2023-05-11 16:14:33 +0700

    Init new package for database app
    Added `dbApp` module that initializes the database

---
+++ dbApp/dbApp.py
@@ -0,0 +1,38 @@
+from .database import databaseSession
+from .sqlQueries import SqlQueries
+from helpers.fileSystem import fileSystem
+from helpers.IDGenerator import g_IDGenerator
+
+
+class DatabaseInitializer:
+    @classmethod
+    def run(cls):
+        operations = [
+            SqlQueries.createDatabase(),
+            SqlQueries.createProductsTable(),
+            SqlQueries.createTableOrders()
+        ]
+        with databaseSession as db:
+            for operation in operations:
+                db.execute(operation)
+        cls.__createProducts()
+
+    @staticmethod
+    def __createProducts():
+        for file in fileSystem.listDir("./data/products/", suffix="json"):
+            data = fileSystem.readJson(file)
+            name = data["name"]
+            description = data["description"]
+            cost = float(data["cost"])
+            image = fileSystem.convertImageToBytes(data["image"])
+            data["image"] = image
+            with databaseSession as db:
+                db.execute(
+                    SqlQueries.insertProduct(
+                        name=name,
+                        description=description,
+                        cost=cost,
+                        image=image
+                    ),
+                    data
+                )
---
+++ dbApp/sqlQueries.py
@@ -0,0 +1,43 @@
+class SqlQueries:
+    @staticmethod
+    def createDatabase():
+        return """CREATE DATABASE IF NOT EXISTS `storage`;"""
+
+    @staticmethod
+    def createProductsTable():
+        return """
+        CREATE TABLE IF NOT EXISTS `storage`.`products` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(255) NOT NULL,
+        `description` TEXT(500) NOT NULL,
+        `cost` FLOAT NOT NULL,
+        `image` LONGBLOB NOT NULL,
+        PRIMARY KEY (`id`));
+        """
+
+    @staticmethod
+    def createTableOrders():
+        return """
+        CREATE TABLE IF NOT EXISTS `storage`.`orders` (
+        `id` VARCHAR(6) NOT NULL,
+        `cost` FLOAT NOT NULL,
+        `productIDs` JSON NOT NULL,
+        PRIMARY KEY (`id`));
+        """
+
+    @staticmethod
+    def selectAllProducts():
+        return """SELECT * FROM `storage`.`products`"""
+
+    @staticmethod
+    def insertProduct(name, description, cost, image):
+        return """
+        INSERT INTO `storage`.`products`
+            (name, description, cost, image)
+        SELECT
+            %(name)s, %(description)s, %(cost)s, %(image)s
+        WHERE
+            NOT EXISTS (
+                SELECT name FROM `storage`.`products` WHERE name = %(name)s
+            )
+        """
